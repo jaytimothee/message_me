dfca95a545d40a4a4c35301078673c95af2dfde8
job timothee <timotheej@outlook.com>
2021-08-30 19:57:20 -0400

    chat application

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,9 @@ Turbolinks.start()
 ActiveStorage.start()
 
 
-$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
+$(document).on('turbolinks:load', function () {
+    $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function () {
+        $(this).closest('.message').transition('fade');
+    });
+})
